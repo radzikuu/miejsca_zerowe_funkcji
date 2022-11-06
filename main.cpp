966e9866700b4ea56e89e60ce14bc9d264d42bbb
Rafał Radzik <115455792+radzikuu@users.noreply.github.com>
2022-11-06 17:57:32 +0100

    v2.0

--- main.cpp
+++ main.cpp
@@ -1,39 +1,48 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
-double a, b, c, x;
+double a, b, c, delta, x1, x2;
 
 void f_liniowa(){
-    cout << "Podaj wspolczynniki a, b, c funkcji liniowej: ";
-
-		cout << "\na = ";
-        cin >> a;
-
-        cout << "b = ";
-        cin >> b;
+    cout << "FUNKCJA KWADRATOWA  ax^2 + bx + c = 0";
+    
+	cout << "\na = ";
+    cin >> a;
+    
+    cout << "b = ";
+    cin >> b;
+      
+	cout << "c = ";
+    cin >> c;
         
-        cout << "c = ";
-        cin >> c;
-        
 
-    if(a==0){
-		if(b==0){
-			if(c==0){
-				cout << "Rownanie ma nieskonczenie wiele rozwiazan";
-			} else {
-				cout << "Rownanie jest sprzeczne";
-			}
-		} else {
-			if(c==0){
-				cout << "Rownanie ma nieskonczenie wiele rozwiazan";	
-			} else {
-				cout << "Rownanie nie ma miejsc zerowych";
-			}
+    if(a > 0 || a < 0){
+		int pow_b = pow(b,2);
+		delta = pow_b - 4 * a * c;
+		int sqrt_d = sqrt(delta);
+		
+		cout << "\nDelta: " << delta << endl;
+				
+		if(delta > 0){
+			
+			x1 = (-b - sqrt_d) / 2*a;
+			x2 = (-b + sqrt_d) / 2*a;
+			
+			cout << "x1 = " << x1;
+			cout << "\nx2 = " << x2;
+		
+		} else if(delta == 0){
+			
+			x1 = -b / 2*a;
+			cout << "x1 = " << x1;
+			
+		} else if(delta < 0){
+			
+			cout << "Funkcja nie ma miejsc zerowych.";
+			
 		}
-	} else {
-		int x;
-		x = -c/a;
-		cout << "Rownanie ma jedno miejsce zerowe: " << x;
+	
 	}
 
     

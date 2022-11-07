135def1c507acf95fd47cd30808ebb9587ac851b
Jan Kowalski <jan@kowalski.com>
2022-11-07 10:22:57 +0100

    v2.6

--- main.cpp
+++ main.cpp
@@ -8,129 +8,128 @@ int wybor;
 
 void licz_funkcje(){
 	cout << "Ktora funkcje chcesz obliczyc?" << endl;
-	
+
 	cout << "1) FUNKCJA LINIOWA ax + b" << endl;
     cout << "2) FUNKCJA KWADRATOWA  ax^2 + bx + c" << endl;
     cout << "3) FUNKCJA KWADRATOWA  a(x-p)^2 + q" << endl;
-    
+
     cout << "\nWybieram: ";
     cin >> wybor;
-    
+
     if(wybor == 1){
     	cout << "\nObliczam: FUNKCJA LINIOWA" << endl;
-	
+
 		cout << "\na = ";
     	cin >> a;
-    
+
     	cout << "b = ";
-    	cin >> b;	
-		
+    	cin >> b;
+
 		x1 = -b/a;
-			
+
 		if(a > 0){
 			cout << "Miejsce zerowe: " << x1;
 			cout << "\nFunkcja jest rosnaca";
-			
+
 		} else if(a == 0){
 			cout << "Nie ma miejsca zerowego";
 			cout << "\nFunkcja jest stala";
-			
+
 		} else if(a < 0){
 			cout << "Miejsce zerowe: " << x1;
 			cout << "\nFunkcja jest malejaca";
 		}
-    	
+
 	}else if(wybor == 2){
 		cout << "\nObliczam: FUNKCJA KWADRATOWA - postac ogolna" << endl;
-    	
+
     	cout << "\na = ";
     	cin >> a;
-    
+
     	cout << "b = ";
     	cin >> b;
-    	
+
     	cout << "c = ";
     	cin >> c;
-    
+
     	if(a > 0 || a < 0){
 			int pow_b = pow(b,2);
 			delta = pow_b - 4 * a * c;
 			double sqrt_d = sqrt(delta);
-			
+
 			cout << "\nDelta: " << delta << endl;
-					
+
 			if(delta > 0){
 				x1 = (-b - sqrt_d) / (2*a);
 				x2 = (-b + sqrt_d) / (2*a);
-				
-				cout << "x1 = " << x1; 
-				cout << "\nx2 = " << x2;
-			
+
+				cout << "x1 = " << x1;
+				cout << "\nx2 = " << x2 << endl;
+
 			} else if(delta == 0){
 				x1 = -b / (2*a);
 				cout << "x1 = " << x1;
-				
+
 			} else if(delta < 0){
-				cout << "Funkcja nie ma miejsc zerowych.";	
-			}	
+				cout << "Funkcja nie ma miejsc zerowych.\n";
+			}
 		}
-		
+
 		else if(a == 0){
 			cout << "\nFUNKCJA MA POSTAC LINIOWA: \n\n";
 			x1 = -c/b;
-			
+
 			if(b > 0){
 				cout << "Miejsce zerowe: " << x1;
-				cout << "\nFunkcja jest rosnaca";
-			
+				cout << "\nFunkcja jest rosnaca\n";
+
 			} else if(b == 0){
 				cout << "Nie ma miejsca zerowego";
-				cout << "\nFunkcja jest stala";
-			
+				cout << "\nFunkcja jest stala\n";
+
 			} else if(b < 0){
 				cout << "Miejsce zerowe: " << x1;
-				cout << "\nFunkcja jest malejaca";
-			}	
+				cout << "\nFunkcja jest malejaca\n";
+			}
 		}
 	}
-	
+
 	else if(wybor == 3){
 		cout << "\nObliczam: FUNKCJA KWADRATOWA - postac kanoniczna" << endl;
-		
-		cout << "";
+
 		do{
 			try{
 				cout << "\na = ";
     			cin >> a;
-    
+
     			cout << "p = ";
     			cin >> p;
-    	
+
     			cout << "q = ";
     			cin >> q;
-				
+
 				if((-q/a) <= 0){
 					string napis = "\n-q/a musi byc wieksze od 0!\n\n";
 					throw(napis);
-				}	
+				}
 			}
-			
+
 			catch(string blad){
 				cout << blad;
 			}
-			
+
 		}while((-q/a) <= 0);
-		
-		if(a > 0 || a < 0){	
+
+		if(a > 0 || a < 0){
 			x1 = p + sqrt(-q/a);
 			x2 = p - sqrt(-q/a);
-			
+
 			cout << "\nx1  = " << x1;
-			cout << "\nx2 = " << x2;
-		
+			cout << "\nx2 = " << x2 << endl;
+
 		}else if(a == 0){
-			cout << "\nFUNKCJA MA POSTAC LINIOWA";
-		}	
+			cout << "\nFUNKCJA MA POSTAC LINIOWA\n";
+		}
 	}
 }
 
